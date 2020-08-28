6fef3c57130b4fee0da30a4a6baf95f1b4727821
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-08-28 13:07:16 +0600

    Updates

---
+++ precache-manifest.11bfa26e63f0ec0cf9365e7e40244faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db84adbcdf10475a5bb09b762d66bde8",
+    "url": "/12-mini-socialmedia/index.html"
+  },
+  {
+    "revision": "4096ec95ce1722fbe4f4",
+    "url": "/12-mini-socialmedia/static/css/main.c604b5e6.chunk.css"
+  },
+  {
+    "revision": "7ff01c69f7884caf2255",
+    "url": "/12-mini-socialmedia/static/js/2.294f7613.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/12-mini-socialmedia/static/js/2.294f7613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4096ec95ce1722fbe4f4",
+    "url": "/12-mini-socialmedia/static/js/main.46deb1b2.chunk.js"
+  },
+  {
+    "revision": "edd7924f9f282254bba7",
+    "url": "/12-mini-socialmedia/static/js/runtime-main.cd0d66c4.js"
+  }
+]);
